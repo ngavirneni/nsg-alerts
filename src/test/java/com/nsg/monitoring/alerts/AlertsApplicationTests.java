d83b0c75e356a6186aafefc8196b8e46d83e08d8
Sreeram Gavirneni <sreeramgavirneni@gmail.com>
2019-03-19 22:13:12 -0400

    InitialCommit"

---
+++ src/test/java/com/nsg/monitoring/alerts/AlertsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nsg.monitoring.alerts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlertsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
